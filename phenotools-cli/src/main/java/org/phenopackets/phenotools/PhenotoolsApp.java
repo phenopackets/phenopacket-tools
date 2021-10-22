a643341f28a9482c94e8320ce50c1891e265157b
Jules Jacobsen <jules.jacobsen@gmail.com>
2021-10-22 16:59:36 +0100

    Revert "Develop"

--- phenotools-cli/src/main/java/org/phenopackets/phenotools/PhenotoolsApp.java
+++ phenotools-cli/src/main/java/org/phenopackets/phenotools/PhenotoolsApp.java
@@ -1,7 +1,7 @@
 package org.phenopackets.phenotools;
 
 
-import org.phenopackets.phenotools.command.ConvertCommand;
+
 import org.phenopackets.phenotools.command.ExamplesCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,8 +10,8 @@ import picocli.CommandLine;
 
 @CommandLine.Command(name = "Phenotools builder", version = "0.0.1", mixinStandardHelpOptions = true)
 public class PhenotoolsApp implements Runnable {
-
     private static final Logger LOGGER = LoggerFactory.getLogger(PhenotoolsApp.class);
+
 
     public static void main(String[] args) {
         if (args.length == 0) {
@@ -19,12 +19,12 @@ public class PhenotoolsApp implements Runnable {
             args = new String[]{"-h"};
         }
         CommandLine cline = new CommandLine(new PhenotoolsApp())
-                .addSubcommand("examples", new ExamplesCommand())
-                .addSubcommand("convert", new ConvertCommand());
+                .addSubcommand("examples", new ExamplesCommand());
         cline.setToggleBooleanFlags(false);
         int exitCode = cline.execute(args);
         System.exit(exitCode);
     }
+
 
     @Override
     public void run() {
