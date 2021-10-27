8bbc2d87e9bd8a1db33fcce1a14c805fe4cfcc61
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2021-10-27 17:14:33 +0100

    Update cli convert to use paths for inout and output Attempt to enable autocompletion for the cli

--- phenotools-cli/src/main/java/org/phenopackets/phenotools/PhenotoolsApp.java
+++ phenotools-cli/src/main/java/org/phenopackets/phenotools/PhenotoolsApp.java
@@ -3,10 +3,22 @@ package org.phenopackets.phenotools;
 
 import org.phenopackets.phenotools.command.ConvertCommand;
 import org.phenopackets.phenotools.command.ExamplesCommand;
+import picocli.AutoComplete.GenerateCompletion;
 import picocli.CommandLine;
 
+import static picocli.CommandLine.*;
 
-@CommandLine.Command(name = "phenotools", version = "0.0.1", mixinStandardHelpOptions = true)
+
+@Command(name = "phenotools",
+        version = "0.0.1",
+        mixinStandardHelpOptions = true,
+        subcommands = {
+            // see https://picocli.info/autocomplete.html
+            GenerateCompletion.class,
+            ExamplesCommand.class,
+            ConvertCommand.class
+        }
+)
 public class PhenotoolsApp implements Runnable {
 
     public static void main(String[] args) {
@@ -14,11 +26,8 @@ public class PhenotoolsApp implements Runnable {
             // if the user doesn't pass any command or option, add -h to show help
             args = new String[]{"-h"};
         }
-        CommandLine cline = new CommandLine(new PhenotoolsApp())
-                .addSubcommand("examples", new ExamplesCommand())
-                .addSubcommand("convert", new ConvertCommand())
-                .setExpandAtFiles(true)
-                .setToggleBooleanFlags(false);
+        CommandLine cline = new CommandLine(new PhenotoolsApp());
+        cline.getSubcommands().get("generate-completion").getCommandSpec().usageMessage().hidden(true);
         int exitCode = cline.execute(args);
         System.exit(exitCode);
     }
