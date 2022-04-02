3839bdd498f40268721ad452076f18e478586b9d
Peter Robinson <peter.robinson@jax.org>
2022-04-02 18:21:27 -0400

    update

--- phenotools-cli/src/main/java/org/phenopackets/phenotools/examples/Retinoblastoma.java
+++ phenotools-cli/src/main/java/org/phenopackets/phenotools/examples/Retinoblastoma.java
@@ -90,7 +90,7 @@ public class Retinoblastoma {
         vibuilder.actionable();
 
 
-        GenomicInterpretationBuilder gbuilder = GenomicInterpretationBuilder.create("interpretation.1");
+        GenomicInterpretationBuilder gbuilder = GenomicInterpretationBuilder.create("interpretation.2");
         gbuilder.causative();
         gbuilder.variantInterpretation(vibuilder);
 
@@ -110,7 +110,7 @@ public class Retinoblastoma {
         vibuilder.pathogenic();
         vibuilder.actionable();
 
-        GenomicInterpretationBuilder gbuilder = GenomicInterpretationBuilder.create("interpretation.2");
+        GenomicInterpretationBuilder gbuilder = GenomicInterpretationBuilder.create("interpretation.1");
         gbuilder.causative();
         gbuilder.variantInterpretation(vibuilder);
 
