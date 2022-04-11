d3c909d5cad78dfb126fe76c6d07f9899a7b0b8a
Peter Robinson <peter.robinson@jax.org>
2022-04-11 14:14:56 -0400

    family builder

---
+++ phenotools-builder/src/main/java/org/phenopackets/phenotools/builder/builders/FamilyBuilder.java
@@ -0,0 +1,14 @@
+package org.phenopackets.phenotools.builder.builders;
+
+import org.phenopackets.schema.v2.Family;
+
+public class FamilyBuilder {
+
+    Family.Builder builder;
+
+    private FamilyBuilder() {
+        builder = Family.newBuilder();
+    }
+
+
+}
---
+++ phenotools-cli/src/main/java/org/phenopackets/phenotools/examples/FamilyWithPedigree.java
@@ -0,0 +1,22 @@
+package org.phenopackets.phenotools.examples;
+
+
+import org.phenopackets.schema.v2.Family;
+
+/**
+ * An example of creating a Family message with pedigree.
+ *
+ */
+public class FamilyWithPedigree {
+
+    private final Family family;
+
+    FamilyWithPedigree() {
+
+
+
+
+    }
+
+
+}
