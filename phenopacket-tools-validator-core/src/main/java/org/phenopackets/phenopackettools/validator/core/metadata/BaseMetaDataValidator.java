8c90e2601729cc296cba4194e2ee43fb22efa0d2
Peter Robinson <peter.robinson@jax.org>
2022-09-21 14:23:28 -0400

    refactoring validation method to superclass

--- phenopacket-tools-validator-core/src/main/java/org/phenopackets/phenopackettools/validator/core/metadata/BaseMetaDataValidator.java
+++ phenopacket-tools-validator-core/src/main/java/org/phenopackets/phenopackettools/validator/core/metadata/BaseMetaDataValidator.java
@@ -2,10 +2,19 @@ package org.phenopackets.phenopackettools.validator.core.metadata;
 
 import com.google.protobuf.MessageOrBuilder;
 import org.phenopackets.phenopackettools.validator.core.PhenopacketValidator;
+import org.phenopackets.phenopackettools.validator.core.ValidationLevel;
 import org.phenopackets.phenopackettools.validator.core.ValidationResult;
 import org.phenopackets.phenopackettools.validator.core.ValidatorInfo;
+import org.phenopackets.schema.v2.core.MetaData;
+import org.phenopackets.schema.v2.core.OntologyClass;
+import org.phenopackets.schema.v2.core.Resource;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.phenopackets.phenopackettools.validator.core.ValidatorInfo.inputValidator;
 
 abstract public class BaseMetaDataValidator<T extends MessageOrBuilder> implements PhenopacketValidator<T> {
 
@@ -18,6 +27,39 @@ abstract public class BaseMetaDataValidator<T extends MessageOrBuilder> implemen
     public ValidatorInfo validatorInfo() {
         return VALIDATOR_INFO;
     }
+
+
+    protected Set<String> getValidOntologyPrefixes(MetaData metaData) {
+        return metaData
+                .getResourcesList()
+                .stream()
+                .map(Resource::getNamespacePrefix)
+                .collect(Collectors.toSet());
+    }
+
+    protected List<ValidationResult> validateOntologyPrefixes(List<OntologyClass> instances,
+                                                              MetaData metaData) {
+        List<ValidationResult> results = new ArrayList<>();
+        Set<String> validOntologyPrefixes = getValidOntologyPrefixes(metaData);
+        for (OntologyClass instance: instances) {
+            String id = instance.getId();
+            String [] fields =  id.split("[:_]");
+            if (fields.length != 2) {
+                results.add(ValidationResult.of(inputValidator(), ValidationLevel.ERROR,
+                        "Term ID syntax", // TODO ????????????????????
+                        String.format("Malformed ontology term: \"%s\"", id)));
+            }
+            String termPrefix = fields[0];
+            if (! validOntologyPrefixes.contains(termPrefix)) {
+                results.add(ValidationResult.of(inputValidator(), ValidationLevel.ERROR,
+                        "Ontology Not In MetaData", // TODO ????????????????????
+                        String.format("No ontology corresponding to term: \"%s\" found in MetaData", id)));
+            }
+
+        }
+        return results;
+    }
+
 
 
 }
--- phenopacket-tools-validator-core/src/main/java/org/phenopackets/phenopackettools/validator/core/metadata/PhenopacketMetaDataValidator.java
+++ phenopacket-tools-validator-core/src/main/java/org/phenopackets/phenopackettools/validator/core/metadata/PhenopacketMetaDataValidator.java
@@ -1,16 +1,11 @@
 package org.phenopackets.phenopackettools.validator.core.metadata;
 
-import com.google.protobuf.Descriptors;
-import org.phenopackets.phenopackettools.validator.core.ValidationLevel;
 import org.phenopackets.phenopackettools.validator.core.ValidationResult;
 import org.phenopackets.schema.v2.PhenopacketOrBuilder;
-import org.phenopackets.schema.v2.core.MetaData;
 import org.phenopackets.schema.v2.core.OntologyClass;
-import org.phenopackets.schema.v2.core.Resource;
 
 import java.util.*;
 
-import static org.phenopackets.phenopackettools.validator.core.ValidatorInfo.inputValidator;
 
 public class PhenopacketMetaDataValidator extends BaseMetaDataValidator<PhenopacketOrBuilder>  {
 
@@ -18,37 +13,20 @@ public class PhenopacketMetaDataValidator extends BaseMetaDataValidator<Phenopac
 
     @Override
     public List<ValidationResult> validate(PhenopacketOrBuilder phenopacket) {
-        List<ValidationResult> results = new ArrayList<>();
-        MetaData metaData = phenopacket.getMetaData();
-        List<OntologyClass> instances = new ArrayList<>();
-        Map<Descriptors.FieldDescriptor, Object> allFields = phenopacket.getAllFields();
-        for (var e : allFields.entrySet()) {
-            Descriptors.FieldDescriptor fdesc = e.getKey();
-            if (fdesc.getName().equals("OntologyClass")) {
-                instances.add((OntologyClass) e.getValue());
-            }
-        }
-        Set<String> validOntologyPrefixes = new HashSet<>();
-        for (Resource resource : metaData.getResourcesList()) {
-            validOntologyPrefixes.add(resource.getNamespacePrefix());
-        }
-        for (OntologyClass instance: instances) {
-            String id = instance.getId();
-            String [] fields =  id.split("[:_]");
-            if (fields.length != 2) {
-                results.add(ValidationResult.of(inputValidator(), ValidationLevel.ERROR,
-                        "Term ID syntax", // TODO ????????????????????
-                        String.format("Malformed ontology term: \"%s\"", id)));
-            }
-            String termPrefix = fields[0];
-            if (! validOntologyPrefixes.contains(termPrefix)) {
-                results.add(ValidationResult.of(inputValidator(), ValidationLevel.ERROR,
-                        "Ontology Not In MetaData", // TODO ????????????????????
-                        String.format("No ontology corresponding to term: \"%s\" found in MetaData", id)));
-            }
+        // get all fields of the Phenopacket
+        // filter fields that are instances of OntologyClass
+        // cast the objects to OntologyClass
 
-        }
-
-        return results;
+        List<OntologyClass> instances = phenopacket
+                .getAllFields()
+                .entrySet()
+                .stream()
+                .filter(e -> e.getKey().getName().equals("OntologyClass") )
+                .map(Map.Entry::getValue)
+                .map(OntologyClass.class::cast)
+                .toList();
+        // validate that these fields use ontology prefixes that are represented in the MetaData
+        // section using a superclass method
+        return validateOntologyPrefixes(instances, phenopacket.getMetaData());
     }
 }
