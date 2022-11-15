c2db8da285105e6d6de9090ecf4a592c1752b3b7
Daniel Danis <daniel.gordon.danis@protonmail.com>
2022-11-15 13:16:05 -0500

    Add javadocs for the `test` module.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

---
+++ phenopacket-tools-test/src/main/java/org/phenopackets/phenopackettools/test/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A package with {@link org.phenopackets.phenopackettools.test.TestData} - a static utility class for serving
+ * hard-coded phenopacket examples.
+ */
+package org.phenopackets.phenopackettools.test;
