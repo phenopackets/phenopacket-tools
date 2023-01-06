05cb4d368eb2f76ac9eebc5cdb856348f30101e3
Daniel Danis <daniel.gordon.danis@protonmail.com>
2023-01-06 17:35:10 -0500

    Add unspecified zygosity into AllelicState constants.
    Signed-off-by: Daniel Danis <daniel.gordon.danis@protonmail.com>

--- phenopacket-tools-builder/src/main/java/org/phenopackets/phenopackettools/builder/constants/AllelicState.java
+++ phenopacket-tools-builder/src/main/java/org/phenopackets/phenopackettools/builder/constants/AllelicState.java
@@ -9,10 +9,12 @@ public class AllelicState {
   private static final OntologyClass HETEROZYGOUS = OntologyClassBuilder.ontologyClass("GENO:0000135", "heterozygous");
   private static final OntologyClass HOMOZYGOUS = OntologyClassBuilder.ontologyClass("GENO:0000136", "homozygous");
   private static final OntologyClass HEMIZYGOUS = OntologyClassBuilder.ontologyClass("GENO:0000134", "hemizygous");
+  private static final OntologyClass UNSPECIFIED_ZYGOSITY = OntologyClassBuilder.ontologyClass("GENO:0000137", "unspecified zygosity");
 
 
   public static OntologyClass heterozygous() { return HETEROZYGOUS; }
   public static OntologyClass homozygous() { return HOMOZYGOUS; }
   public static OntologyClass hemizygous() { return HEMIZYGOUS; }
+  public static OntologyClass unspecifiedZygosity() { return UNSPECIFIED_ZYGOSITY; }
 
 }
